48437e50ab52ceb4160dc4aa13defce72ab962a2
Akash <akashniki@gmail.com>
2020-04-30 11:15:31 +0200

    Revert "cepheus: Switch to OSS camera stack"
    This reverts commit 7647441df32f49d7e7dde3b92193204baed5a49f.
    
    Change-Id: I42db7b3be889fcc77877d2de17bfa64b7462d695

--- msmnile.mk
+++ msmnile.mk
@@ -129,8 +129,6 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
